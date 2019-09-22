5f736584e8091a77750581879b710b1d58bbe9ad
Kirin Patel <kirinpatel@gmail.com>
2019-09-22 14:35:25 -0400

    Started work on Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,28 @@
 import React, { Component } from "react";
+import { Card, CardContent, TextField, Typography } from "@material-ui/core";
 
 export default class extends Component {
   render() {
-    return <div />;
+    return (
+      <div
+        style={{
+          minHeight: "calc(100vh - 10)",
+          maxWidth: "100%",
+          margin: 10
+        }}
+      >
+        <Card raised={true}>
+          <CardContent>
+            <Typography variant="h1">lightroom</Typography>
+            <TextField
+              label="Bridge Address"
+              margin="normal"
+              variant="outlined"
+              fullWidth={true}
+            />
+          </CardContent>
+        </Card>
+      </div>
+    );
   }
 }
