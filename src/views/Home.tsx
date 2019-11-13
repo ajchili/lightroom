360bf5f41c18065505dfef3f5c54ab8dcbe955f7
Kirin Patel <kirinpatel@gmail.com>
2019-11-12 23:53:11 -0500

    Added react & ssr

---
+++ src/views/Home.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class extends Component {
+  public render(): JSX.Element {
+    return <div>
+      <h1>Hello, World!</h1>
+    </div>;
+  }
+}
