e4c0ee83f2cba8f6019dd98b34b2b60dd540f234
Kirin Patel <kirinpatel@gmail.com>
2019-12-01 16:15:20 -0500

    Removed react

--- src/views/Home.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default class extends Component {
-  public render(): JSX.Element {
-    return <div>
-      <h1>Hello, World!</h1>
-    </div>;
-  }
-}
